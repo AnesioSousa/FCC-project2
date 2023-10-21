3046fa146ce9eaf4b9de8c4b3d41226934c4257d
Anésio Neto <anesios98@gmail.com>
2023-10-20 21:46:14 -0300

    feat: Updates on the interface: the locations are dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
 import Location from "./components/Location";
 import world from "./img/Path.png";
 
+const locations = [
+  {
+    url: "https://source.unsplash.com/WLxQvbMyfas",
+    country: "Japan",
+    place: "Mount Fuji",
+    season: "12 Jan, 2021 - 24 Jan, 2021",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+  },
+  {
+    url: "https://source.unsplash.com/JmuyB_LibRo",
+    country: "Australia",
+    place: "Sydney Opera House",
+    season: "27 May, 2021 - 8 Jun, 2021",
+    description:
+      "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+  },
+  {
+    url: "https://source.unsplash.com/3PeSjpLVtLg",
+    country: "Norway",
+    place: "Geirangerfjord",
+    season: "01 Oct, 2021 - 18 Nov, 2021",
+    description:
+      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+  },
+];
+
 function App() {
   return (
     <div className="flex justify-center items-center ">
@@ -17,7 +44,16 @@ function App() {
 
         {/*LISTA*/}
         <div className="mt-10">
-          <Location />
+          {locations.map((location, index) => (
+            <Location
+              key={index}
+              url={location.url}
+              country={location.country}
+              place={location.place}
+              season={location.season}
+              description={location.description}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,33 +1,31 @@
 import mapIcon from "../img/Fill219.png";
 
-function Location() {
+function Location({ url, country, place, season, description }) {
   return (
     <div className="flex">
       <img
         className="ml-10 h-[225px] w-[168px] rounded-lg"
-        src="https://source.unsplash.com/WLxQvbMyfas"
-        alt="Mount Fuji"
+        src={url}
+        alt={country}
       />
       <div className="ml-5 mt-2 mr-10">
-        <div className="flex justify-start items-center">
-          <img
-            className="h-[9.55px] w-[7px] rounded-lg mr-1"
-            src={mapIcon}
-            alt="Map icon"
-          />
-          <h3 className="mr-3">JAPAN </h3>
-          <a className="underline text-gray-100 cursor-pointer hover:text-cyan-400">
-            View on Google Maps
-          </a>
-        </div>
-        <div>
-          <h1 className="mb-4 font-bold text-4xl">Mount Fuji</h1>
-          <p className="text-sm font-bold">12 Jan, 2021-24 Jan, 2021</p>
-          <p className="text-sm">
-            Mount Fuji is the tallest mountain in Japan, standing at 3,776
-            meters (12,380 feet). Mount Fuji is the single most popular tourist
-            site in Japan, for both Japanese and foreign tourists.
-          </p>
+        <div className="flex flex-col items-start">
+          <div className="flex justify-start items-center">
+            <img
+              className="h-[9.55px] w-[7px] rounded-lg mr-1"
+              src={mapIcon}
+              alt="Map icon"
+            />
+            <h3 className="mr-3">{String(country).toLocaleUpperCase()} </h3>
+            <a className="underline text-gray-100 cursor-pointer hover:text-cyan-400">
+              View on Google Maps
+            </a>
+          </div>
+          <div>
+            <h1 className="mb-4 font-bold text-4xl">{place}</h1>
+            <p className="text-sm font-bold">{season}</p>
+            <p className="text-sm">{description} </p>
+          </div>
         </div>
       </div>
     </div>
